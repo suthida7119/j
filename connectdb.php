b877c0e843603ab0380b01a25e4ad9ac1bdf002d
suthida7119 <66010914085@msu.ac.th>
2026-02-19 17:04:02 +0700

    Update connectdb.php

--- connectdb.php
+++ connectdb.php
@@ -1,9 +1,10 @@
 <?php
         $host = "localhost";
         $user = "root";
-        $pwd = "";
+        $pwd = "915800";
         $db = "4085db";
         $conn = mysqli_connect($host,$user,$pwd,$db) or die ("เชื่อมต่อฐานข้อมูลไม่ได้");
         mysqli_query($conn,"SET NAMES utf8");
+
 
 ?>
